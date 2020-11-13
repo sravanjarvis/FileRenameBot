8c4bece21f471c636f12aefa95ec7d8a4854c8ce
sravanjarvis <68535329+sravanjarvis@users.noreply.github.com>
2020-11-14 00:34:45 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -39,6 +39,15 @@ async def rename_doc(bot, update):
     if update.from_user.id in Config.BANNED_USERS:
         await update.reply_text("You are B A N N E D")
         return
+    try:
+       chat = await bot.get_chat_member(CHANNEL_USERNAME, chat_id)
+       if chat.status=='kicked'
+           if edit_message:
+               await reply('You were Banned')
+           return False
+       else:
+           return True
+     except usernotparticipant
     TRChatBase(update.from_user.id, update.text, "rename")
     if (" " in update.text) and (update.reply_to_message is not None):
         cmd, file_name = update.text.split(" ", 1)
